ea818223d51adee312332b44827e4407fb0b4259
nearnshaw <nearnshaw@decentraland.org>
2021-05-03 16:40:08 -0300

    fix rotation bug

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,3 @@
-
 // Create path curve
 
 // how many points on the curve
@@ -14,16 +13,20 @@ const cpoint4 = new Vector3(3.2, 3.2, 11.2)
 const cpoints = [cpoint1, cpoint2, cpoint3, cpoint4]
 
 // Create a Catmull-Rom Spline curve. It that passes through all 4 points. The total number of points in the path is set by  `curvePoints`
-let catmullPath = Curve3.CreateCatmullRomSpline(cpoints, curvePoints, true).getPoints()
+let catmullPath = Curve3.CreateCatmullRomSpline(
+  cpoints,
+  curvePoints,
+  true
+).getPoints()
 
 // Custom component to store path and lerp data
-@Component("pathData")
+@Component('pathData')
 export class PathData {
   origin: number = 0
   target: number = 1
   path: Vector3[] = catmullPath
   fraction: number = 0
-  constructor(path: Vector3[]){
+  constructor(path: Vector3[]) {
     this.path = path
   }
 }
@@ -32,24 +35,23 @@ export class PathData {
 export const sharks = engine.getComponentGroup(PathData)
 
 // Custom component to store rotational lerp data
-@Component("rotateData")
+@Component('rotateData')
 export class RotateData {
-  originRot: Quaternion
-  targetRot: Quaternion
+  originRot: Quaternion = Quaternion.Identity
+  targetRot: Quaternion = Quaternion.Identity
   fraction: number = 0
 }
 
 // Custom component to store current speed
-@Component("swimSpeed")
+@Component('swimSpeed')
 export class SwimSpeed {
   speed: number = 0.5
 }
 
-
 // Lerp over the points of the curve
 export class PatrolPath {
   update() {
-    for (let shark of sharks.entities){
+    for (let shark of sharks.entities) {
       let transform = shark.getComponent(Transform)
       let path = shark.getComponent(PathData)
       let speed = shark.getComponent(SwimSpeed)
@@ -57,17 +59,16 @@ export class PatrolPath {
         path.path[path.origin],
         path.path[path.target],
         path.fraction
-        )
-      path.fraction += speed.speed/10
+      )
+      path.fraction += speed.speed / 10
       if (path.fraction > 1) {
         path.origin = path.target
         path.target += 1
-        if (path.target >= path.path.length-1) {
+        if (path.target >= path.path.length - 1) {
           path.target = 0
         }
-        path.fraction = 0      
+        path.fraction = 0
       }
-     
     }
   }
 }
@@ -77,23 +78,24 @@ engine.addSystem(new PatrolPath(), 2)
 // Change speed depending on how steep is the shark's path
 export class UpdateSpeed {
   update() {
-    for (let shark of sharks.entities){
+    for (let shark of sharks.entities) {
       let speed = shark.getComponent(SwimSpeed)
       let path = shark.getComponent(PathData)
-    
-      let depthDiff = (path.path[path.target].y - path.path[path.origin].y) * curvePoints
-      if (depthDiff > 1){
+
+      let depthDiff =
+        (path.path[path.target].y - path.path[path.origin].y) * curvePoints
+      if (depthDiff > 1) {
         depthDiff = 1
-      } else if (depthDiff < -1){
+      } else if (depthDiff < -1) {
         depthDiff = -1
       }
-      depthDiff += 1.5   // from 0.5 to 2.5
-    
-      let clipSwim = shark.getComponent(Animator).getClip("swim")
+      depthDiff += 1.5 // from 0.5 to 2.5
+
+      let clipSwim = shark.getComponent(Animator).getClip('swim')
       clipSwim.speed = depthDiff
       clipSwim.weight = depthDiff
-      
-      speed.speed = ((depthDiff * -1) + 3) // from 2.5 to 0.5
+
+      speed.speed = depthDiff * -1 + 3 // from 2.5 to 0.5
       //log("dd :" , depthDiff, " speed: " , speed.speed)
     }
   }
@@ -104,19 +106,21 @@ engine.addSystem(new UpdateSpeed(), 1)
 // Rotate gradually with a spherical lerp
 export class RotateSystem {
   update(dt: number) {
-    for (let shark of sharks.entities){
+    for (let shark of sharks.entities) {
       let transform = shark.getComponent(Transform)
       let path = shark.getComponent(PathData)
       let rotate = shark.getComponent(RotateData)
       let speed = shark.getComponent(SwimSpeed)
-      rotate.fraction +=  speed.speed/10
+      rotate.fraction += speed.speed / 10
 
       if (rotate.fraction > 1) {
         rotate.fraction = 0
         rotate.originRot = transform.rotation
-        let direction = path.path[path.target].subtract(path.path[path.origin]).normalize()
+        let direction = path.path[path.target]
+          .subtract(path.path[path.origin])
+          .normalize()
         rotate.targetRot = Quaternion.LookRotation(direction)
-      }  
+      }
       transform.rotation = Quaternion.Slerp(
         rotate.originRot,
         rotate.targetRot,
@@ -128,20 +132,19 @@ export class RotateSystem {
 
 engine.addSystem(new RotateSystem(), 3)
 
-
-
-
 // Add Shark model
 let shark = new Entity()
-shark.addComponent(new Transform({
-  position: new Vector3(1, 0, 1),
-  scale: new Vector3(0.5, 0.5, 0.5)
-}))
-shark.addComponent(new GLTFShape("models/shark.glb"))
+shark.addComponent(
+  new Transform({
+    position: new Vector3(1, 0, 1),
+    scale: new Vector3(0.5, 0.5, 0.5),
+  })
+)
+shark.addComponent(new GLTFShape('models/shark.glb'))
 shark.addComponent(new Animator())
 
 // Add animations
-const clipSwim3 = new AnimationState("swim")
+const clipSwim3 = new AnimationState('swim')
 clipSwim3.speed = 0.5
 clipSwim3.weight = 0.5
 shark.getComponent(Animator).addClip(clipSwim3)
@@ -157,14 +160,14 @@ shark.addComponent(new SwimSpeed())
 // Add shark to engine
 engine.addEntity(shark)
 
-
 // Add 3D model for scenery
 const seaBed = new Entity()
-seaBed.addComponent(new GLTFShape("models/Underwater.gltf"))
-seaBed.addComponent(new Transform({
-  position: new Vector3(8, 0, 8),
-  rotation: Quaternion.Euler(0, 90, 0),
-  scale: new Vector3(0.8, 0.8, 0.8)
-
-}))
+seaBed.addComponent(new GLTFShape('models/Underwater.gltf'))
+seaBed.addComponent(
+  new Transform({
+    position: new Vector3(8, 0, 8),
+    rotation: Quaternion.Euler(0, 90, 0),
+    scale: new Vector3(0.8, 0.8, 0.8),
+  })
+)
 engine.addEntity(seaBed)
